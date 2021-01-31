21ff7c3a9ebcac93e841c6315c73e3134c4d7c98
Yissachar Shechter <upsilon01@gmail.com>
2021-01-31 05:35:24 +0200

    secondary-commit

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import * as fetch from 'node-fetch';
+import * as f from 'node-fetch';
+import { Response } from 'node-fetch';
 import { range } from 'rxjs';
-import { switchMap, tap } from 'rxjs/operators';
+import { switchMap, tap, mergeMap, map } from 'rxjs/operators';
 
-function request(): Promise<fetch.Response> {
-  return fetch.default(
+const fetch = f.default;
+
+function request(): Promise<Response> {
+  return fetch(
     'https://api-2-0.spot.im/v1.0.0/authenticate',
     {
       headers: {
@@ -27,10 +30,53 @@ function request(): Promise<fetch.Response> {
 
 range(0, 10)
   .pipe(
-      switchMap(x => request()),
-      switchMap(res => res.headers.entries()),
-      tap(res => {
+    mergeMap(x => request()),
+    map(res => res.headers.entries()),
+    map(x => Array.from(x)),
+    mergeMap(res => {
+const d = OperationHeaders.setFields(res as any);
+     return like(OperationHeaders.setFields(res as any))
+    
+    })
+  ).subscribe(e => console.log(e))
 
-        console.log(res)
-      })
-  ).subscribe()
+
+function like(instance: OperationHeaders) {
+
+  return fetch("https://api-2-0.spot.im/v1.0.0/rank/rank/message", {
+    "headers": {
+      "accept": "application/json",
+      "accept-language": "he-IL,he;q=0.9",
+      "content-type": "application/json",
+      "sec-ch-ua": "\"Google Chrome\";v=\"87\", \" Not;A Brand\";v=\"99\", \"Chromium\";v=\"87\"",
+      "sec-ch-ua-mobile": "?0",
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "cross-site",
+      ...instance
+    },
+    "referrer": "https://www.globes.co.il/",
+    "referrerPolicy": "strict-origin-when-cross-origin",
+    "body": "{\"operation\":\"like\",\"message_id\":\"sp_oYNOQACf_1001358607_c_1noVXKi1tiFjlOnD96rJUkLcZPD\",\"source\":\"conversation\"}",
+    "method": "POST",
+    "mode": "cors"
+  } as any);
+
+}
+
+
+class OperationHeaders {
+  "x-access-token": string;
+  "x-spot-id": string;
+  "x-spotim-device-uuid": string;
+  "x-spotim-device-v2": string;
+  "x-spotim-page-view-id": string;
+
+  static setFields(params: [keyof OperationHeaders, string][]) {
+    const instance = new OperationHeaders();
+    for (const [key, value] of params) {
+      instance[key] = value;
+    }
+    return instance;
+  }
+}
